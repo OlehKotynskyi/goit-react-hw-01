69932d0248fd83430eb9ea397dab30a6587e3c9f
Oleh Kot <88795955+OlehKotynskyi@users.noreply.github.com>
2024-01-06 15:31:57 +0200

    new project

---
+++ src/components/profile/Profile.jsx
@@ -0,0 +1,41 @@
+import PropTypes from "prop-types";
+
+export const Profile = ({ username, avatar, stats, tag, location }) => {
+  return (
+    <div className="">
+      <div className="description">
+        <img src={avatar} alt={username} className="avatar" />
+        <p className="name">{username}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+      </div>
+
+      <ul className="">
+        <li className="">
+          <span className="label">Followers</span>
+          <span className="quantity">{stats.followers}</span>
+        </li>
+        <li>
+          <span className="label">Views</span>
+          <span className="quantity">{stats.views}</span>
+        </li>
+        <li>
+          <span className="label">Likes</span>
+          <span className="quantity">{stats.likes}</span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }),
+};
